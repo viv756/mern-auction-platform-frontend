d0a49ca1f39b0824c9c1b3f63424096ebe1ad3da
*vivek <vrvivekofficial@gmail.com>
2024-12-24 14:55:53 +0530

    create logout functionality and create sideDrawer

---
+++ src/store/slices/userSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    loading: false,
+    isAuthenticated: false,
+    user: {},
+    leaderboard: [],
+  },
+  reducers: {
+    logoutSuccess(state, action) {
+      state.isAuthenticated = false;
+      state.user = {};
+    },
+    logoutFailed(state, action) {
+      state.loading = false;
+      state.isAuthenticated = state.isAuthenticated;
+      state.user = state.user;
+    },
+    clearAllErrors(state, action) {
+      state.user = state.user;
+      state.isAuthenticated = state.isAuthenticated;
+      state.leaderboard = state.leaderboard;
+      state.loading = false;
+    },
+  },
+});
+
+export const logout = () => async (dispatch) => {
+  try {
+    const response = await axios.get("http://localhost:5000/api/v1/user/logout", {
+      withCredentials: true,
+    });
+    dispatch(userSlice.actions.logoutSuccess());
+    toast.success(response.data.message);
+    dispatch(userSlice.actions.clearAllErrors());
+  } catch (error) {
+    dispatch(userSlice.actions.logoutFailed());
+    toast.error(error.response.data.message);
+    dispatch(userSlice.actions.clearAllErrors());
+  }
+};
+
+export default userSlice.reducer;
