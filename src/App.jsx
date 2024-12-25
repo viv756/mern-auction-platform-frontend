801480d75cfcabb63922dda6d3ca4195131fb77f
*vivek <vrvivekofficial@gmail.com>
2024-12-25 10:54:07 +0530

    create submit commission component and submit commission functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import "react-toastify/dist/ReactToastify.css";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import SubmitCommission from "./pages/SubmitCommission";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/submit-commission" element={<SubmitCommission />} />
       </Routes>
       <ToastContainer position="top-right" />
     </BrowserRouter>
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "@/store/slices/userSlice"
+import userReducer from "@/store/slices/userSlice";
+import commissionReducer from "@/store/slices/commissionSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    commission: commissionReducer,
   },
 });
