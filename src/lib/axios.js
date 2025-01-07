b073996959e56af276cd158fbf22453322a04eef
*vivek <vrvivekofficial@gmail.com>
2025-01-07 13:36:16 +0530

    set up backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1" : "/api",
+  baseURL:import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api/v1"
+      : import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
